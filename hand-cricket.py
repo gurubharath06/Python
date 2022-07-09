ed9ca4a68cd87a3253f6bd051fd55d552cfcefd0
gurubharath06 <98326070+gurubharath06@users.noreply.github.com>
2022-07-09 09:48:32 +0530

    Delete hand-cricket.py

--- hand-cricket.py
+++
@@ -1,7 +0,0 @@
-# a program to play hand cricket game with the computer
-#import all required packages
-import random
-def computer_random_choice():
-    options_available = [0,1,2,3,4,5,6,10,12,20]
-    computer_choice = random.choice(options_available)
-    return computer_choice
